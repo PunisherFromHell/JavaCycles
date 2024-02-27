65309cce33a08e524410941c552eb54f9ef0009a
mega test <mahp0@mail.ru>
2024-02-27 11:28:43 +0800

    parameterized test

--- src/test/java/ru/netology/services/MonthsNumberServiceTest.java
+++ src/test/java/ru/netology/services/MonthsNumberServiceTest.java
@@ -2,15 +2,19 @@ package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class MonthsNumberServiceTest {
-    @Test
-    public void ShouldCalculateNumberOfMonths() {
+    @ParameterizedTest
+    @CsvFileSource(files="src/test/resources/months.csv")
+    public void ShouldCalculateNumberOfMonths(int expectedRestMonths,int income,
+                                              int expenses, int threshold) {
         MonthsNumberService service = new MonthsNumberService();
-        int income = 100_000;
-        int expenses = 60_000;
-        int threshold = 150_000;
-        int expectedRestMonths = 2;
+        //int income = 100_000;
+       // int expenses = 60_000 ;
+       // int threshold =150_000;
+        //int expectedRestMonths = 2;
         int actualRestMonths =
                 MonthsNumberService.numberOfMonths(income, expenses, threshold);
         Assertions.assertEquals(expectedRestMonths, actualRestMonths);
